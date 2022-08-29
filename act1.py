ffbf6627c696328b24afc87f7c2a025617d1017a
ByunE1956 <fpqp85456@naver.com>
2022-08-29 22:26:39 +0900

    Python Exercise Day1 [print]

---
+++ act1.py
@@ -0,0 +1,26 @@
+# print 문제
+
+# 문제1
+print("Hello")  # Hello
+
+# 문제2
+print("Hello World")    # Hello World
+
+# 문제3
+print("Hello")  # Hello
+print("World")  # World
+
+# 문제4
+print("'Hello'")    # 'Hello'
+
+# 문제5
+print("\"Hello World\"")    # "Hello World"
+
+# 문제6
+print("\"!@#$%^&*()\'")     # "!@#$%^&*()'
+
+# 문제7
+print("\"C:\\Download\\\'hello\'.py\"")     # "C:\\Download\\\'hello\'.py\"
+
+# 문제8
+print("print(\"Hello\\nWorld\")")   # print("Hello\nWorld")
