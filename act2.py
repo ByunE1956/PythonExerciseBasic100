abc93668e82112725c70aa041e0d40dbe874e018
ByunE1956 <fpqp85456@naver.com>
2022-08-30 21:32:36 +0900

    Python Exercise Day2 [input]

---
+++ act2.py
@@ -0,0 +1,85 @@
+# 기초 입출력 문제
+
+# 문제1 - 문자 1개 입력받아 출력하기
+#c = input()
+#print(c) 
+
+# 문제2 - 정수 1개 입력받아 int로 변화하여 출력하기
+#n = int(input())
+#print(n)
+
+# 문제3 - 실수 1개 입력받아 변환하여 출력하기
+#f = float(input())
+#print(f)
+
+# 문제4 - 정수 2개 입력받아 그대로 출력하기
+#a = int(input())
+#b = int(input())
+#print(a)
+#print(b)
+
+# 문제4 - 문자 2개 입력받아 순서 바꿔 출력하기
+#a = input()
+#b = input()
+#print(b)
+#print(a)
+
+# 문제5 - 실수 1개 입력받아 3번 출력하기
+#f = float(input())
+#print(f)
+#print(f)
+#print(f)
+
+# 문제6 - 정수 2개 입력받아 그대로 출력하기
+#a, b = input().split()
+#a = int(a)
+#b = int(b)
+#print(a)
+#print(b)
+
+# 문제7 - 문자 2개 입력받아 순서 바꿔 출력하기
+#c1, c2 = input().split()
+#print(c2, c1)
+
+# 문제8 - 문장 1개 입력받아 3번 출력하기
+#s = input()
+#print(s,s,s)
+
+# 문제9 - 시간 입력받아 그대로 출력하기
+#a, b = input().split(':')
+#print(a, b, sep=':')
+
+# 문제10 - 연월일 입력받아 순서바꿔 출력하기
+#y, m, d = input().split('.')
+#print(d, m, y, sep='-')
+
+# 문제11 - 주민번호 입력받아 형태 바꿔 출력하기
+#s, e = input().split('-')
+#print(s, e, sep='')
+
+# 문제12 - 단어 1개 입력받아 나누어 출력하기
+#s = input()
+#for item in s:
+#    print(item)
+
+# 문제13 - 연월일 입력받아 나누어 출력하기
+#s = input()
+#print(s[0:2], s[2:4], s[4:6])
+
+# 문제14 - 시분초 입력받아 분만 출력하기
+#h, m, s = input().split(':')
+#print(m)
+
+# 문제15 - 단어 2개 입력받아 이어 붙이기
+#w1, w2 = input().split()
+#print(w1 + w2)
+
+# 문제16 - 정수 2개 입력받아 합 계산하기
+#a, b = input().split()
+#c = int(a) + int(b)
+#print(c)
+
+# 문제17 - 실수 2개 입력받아 합 계산하기
+f1 = float(input())
+f2 = float(input())
+print(f1 + f2)
