e4ed5b1e67d9c1690c48c0576cf6630301414dc7
ByunE1956 <fpqp85456@naver.com>
2022-09-06 20:26:24 +0900

    Python Exeercise Day4 [기초 산술연산]

--- act5.py
+++ act5.py
@@ -1,66 +1,66 @@
 # 기초 산술연산 문제
 
 # 문제1 - 정수 1개 입력받아 부호바꾸기
-#n = int(input())
-#print(-n)
+n = int(input())
+print(-n)
 
 # 문제2 - 문자 1개 입력받아 다음 문자 출력하기
-#n = ord(input())
-#print(chr(n+1))
+n = ord(input())
+print(chr(n+1))
 
 # 문제3 - 정수 2개 입력받아 차 계산하기
-#a, b = input().split()
-#c = int(a) - int(b)
-#print(c)
+a, b = input().split()
+c = int(a) - int(b)
+print(c)
 
 # 문제4 - 실수 2개 입력받아 곱 계산하기
-#a, b = input().split()
-#c = float(a) * float(b)
-#print(c)
+a, b = input().split()
+c = float(a) * float(b)
+print(c)
 
 # 문제5 - 단어 여러 번 출력하기
-#w, n = input().split()
-#print(w * int(n))
+w, n = input().split()
+print(w * int(n))
 
 # 문제6 - 문장 여러 번 출력하기
-#n = int(input())
-#s = input()
-#print(n * s)
+n = int(input())
+s = input()
+print(n * s)
 
 # 문제7 - 정수 2개 입력받아 거듭제곱 계산하기
-#a, b = input().split()
-#c = int(a) ** int(b)
-#print(c)
+a, b = input().split()
+c = int(a) ** int(b)
+print(c)
 
 # 문제8 - 실수 2개 입력받아 거듭제곱 계산하기
-#f1, f2 = input().split()
-#c = float(f1) ** float(f2)
-#print(c)
+f1, f2 = input().split()
+c = float(f1) ** float(f2)
+print(c)
 
 # 문제9 - 정수 2개 입력받아 나눈 몫 계산하기
-#a, b = input().split()
-#a, b = int(a), int(b)
-#print(a//b)
+a, b = input().split()
+a, b = int(a), int(b)
+print(a//b)
 
 # 문제10 - 정수 2개 입력받아 나눈 나머지 계산하기
-#a, b = input().split()
-#print(int(a) % int(b))
+a, b = input().split()
+print(int(a) % int(b))
 
 # 문제11 - 실수 2개 입력받아 나눈 결과 계산하기
-#f1, f2 = input().split()
-#c = float(f1) / float(f2)
-#print(format(c, ".3f"))
+f1, f2 = input().split()
+c = float(f1) / float(f2)
+print(format(c, ".3f"))
 
 # 문제12 - 정수 2개 입력받아 자동 계산하기
-#a, b = input().split()
-#a=int(a)
-#b=int(b)
-#print(a+b)
-#print(a-b)
-#print(a*b)
-#print(a//b)
-#print(a%b)
-#print(format((float(a) / float(b)), ".2f"))
+a, b = input().split()
+a=int(a)
+b=int(b)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+print(format((float(a) / float(b)), ".2f"))
 #print(round(a/b, 2)) : round => 반올림 함수 But, 주의할 점이 반올림에 대해 정확하진 않음 1.5 => 2, 2.5 => 2 둘 다 2가 반환됨
 
 # 문제13 - 정수 3개 입력받아 합과 평균 출력하기
