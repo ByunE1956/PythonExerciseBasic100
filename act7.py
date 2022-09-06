6ea1e1f84a33db7c02aa6863afb85e68177867a4
ByunE1956 <fpqp85456@naver.com>
2022-09-06 20:28:07 +0900

    Python Exercise Day5 [기초 비교연산]

---
+++ act7.py
@@ -0,0 +1,17 @@
+# 기초 비교연산
+
+# 문제1 - 정수 2개 입력받아 비교하기1
+a, b = input().split()
+print(int(a) < int(b))
+
+# 문제2 - 정수 2개 입력받아 비교하기2
+a, b = input().split()
+print(int(a) == int(b))
+
+# 문제3 - 정수 2개 입력받아 비교하기3
+a, b = input().split()
+print(int(a) <= int(b))
+
+# 문제4 - 정수 2개 입력받아 비교하기4
+a, b = input().split()
+print(int(a) != int(b))
